71c2b45225f2d443d550f979b06c52a26e932a48
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-05-23 12:27:58 +0100

    Renamed the rules schema file.

---
+++ schemas/rules.py
@@ -0,0 +1,50 @@
+from typing import List, Union, Dict
+
+from pydantic import BaseModel
+
+
+class Rule(BaseModel):
+    command: str
+    properties: Dict[str, Union[str, List[str], Dict[str, Union[str, List[str]]]]]
+
+
+class RequestObject(BaseModel):
+    name: str
+    rules: List[List[Rule]]
+
+    class Config:
+        schema_extra = {
+            "example": {"name": "Example2", "rules": [[{"command": "input_prop_equals",
+                                                        "properties": {"input_property": "request_path",
+                                                                       "value": ["v1", "collections", "*"]}},
+                                                       {"command": "input_prop_in",
+                                                        "properties": {"input_property": "company",
+                                                                       "datasource_name": "items",
+                                                                       "datasource_loop_variable": "name"}},
+                                                       {"command": "input_prop_equals",
+                                                        "properties": {
+                                                            "input_property": "request_method",
+                                                            "value": "GET"}}], [
+                                                          {"command": "input_prop_equals",
+                                                           "properties": {"input_property": "request_path",
+                                                                          "value": ["v1", "collections"]}},
+                                                          {"command": "input_prop_equals",
+                                                           "properties": {"input_property": "request_path",
+                                                                          "value": ["v1", "collections",
+                                                                                    "lakes"]}}], [
+                                                          {"command": "input_prop_equals",
+                                                           "properties": {"input_property": "request_path",
+                                                                          "value": ["v1", "collections",
+                                                                                    "*"]}},
+                                                          {"command": "input_prop_equals",
+                                                           "properties": {"input_property": "company",
+                                                                          "value": "geobeyond"}},
+                                                          {"command": "input_prop_in_as",
+                                                           "properties": {"datasource_name": "items",
+                                                                          "datasource_loop_variables": ["name",
+                                                                                                        "everyone"],
+                                                                          "input_properties": [
+                                                                              "preferred_username",
+                                                                              "groupname"]}}]]}
+
+        }
