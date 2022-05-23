1cfcba43dd7c49826c1857524827324d53ed6a8b
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-05-23 17:30:36 +0100

    Added a new policy schema for UPDATE operations.

--- app/schemas/rules.py
+++ app/schemas/rules.py
@@ -1,4 +1,4 @@
-from typing import List, Union, Dict
+from typing import List, Union, Dict, Optional
 
 from pydantic import BaseModel
 
@@ -11,6 +11,48 @@ class Rule(BaseModel):
 class RequestObject(BaseModel):
     name: str
     rules: List[List[Rule]]
+
+    class Config:
+        schema_extra = {
+            "example": {"name": "Example2", "rules": [[{"command": "input_prop_equals",
+                                                        "properties": {"input_property": "request_path",
+                                                                       "value": ["v1", "collections", "*"]}},
+                                                       {"command": "input_prop_in",
+                                                        "properties": {"input_property": "company",
+                                                                       "datasource_name": "items",
+                                                                       "datasource_loop_variable": "name"}},
+                                                       {"command": "input_prop_equals",
+                                                        "properties": {
+                                                            "input_property": "request_method",
+                                                            "value": "GET"}}], [
+                                                          {"command": "input_prop_equals",
+                                                           "properties": {"input_property": "request_path",
+                                                                          "value": ["v1", "collections"]}},
+                                                          {"command": "input_prop_equals",
+                                                           "properties": {"input_property": "request_path",
+                                                                          "value": ["v1", "collections",
+                                                                                    "lakes"]}}], [
+                                                          {"command": "input_prop_equals",
+                                                           "properties": {"input_property": "request_path",
+                                                                          "value": ["v1", "collections",
+                                                                                    "*"]}},
+                                                          {"command": "input_prop_equals",
+                                                           "properties": {"input_property": "company",
+                                                                          "value": "geobeyond"}},
+                                                          {"command": "input_prop_in_as",
+                                                           "properties": {"datasource_name": "items",
+                                                                          "datasource_loop_variables": ["name",
+                                                                                                        "everyone"],
+                                                                          "input_properties": [
+                                                                              "preferred_username",
+                                                                              "groupname"]}}]]}
+
+        }
+
+
+class UpdateRequestObject(BaseModel):
+    name: Optional[str]
+    rules: Optional[List[List[Rule]]]
 
     class Config:
         schema_extra = {
