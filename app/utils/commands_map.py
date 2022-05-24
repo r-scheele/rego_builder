f1f8f84dde534b430db987aba37a6c992eae2e16
r-scheele <abdulrahmanolamilekan88@gmail.com>
2022-05-24 11:30:33 +0100

    fixed merge conflicts

--- app/utils/commands_map.py
+++ app/utils/commands_map.py
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 from .commands import (
     input_prop_equals,
     input_prop_in,
     input_prop_in_as,
     allow_full_access,
 )
-=======
-from .commands import input_prop_equals
-from .commands import input_prop_in
-from .commands import input_prop_in_as
->>>>>>> main
 
 commands_map = {
     "input_prop_equals": input_prop_equals,
