1bc1f656240ad20c867244799fe6b4cc3ad4f958
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-06-17 09:25:44 +0100

    [Updates]
    - Extra changes to the policy database operation methods
    - Cache GitHub configuration for subsequent requests.
    - [TODO] Fix the broken UPDATE method.

--- app/database/policy_database.py
+++ app/database/policy_database.py
@@ -2,8 +2,6 @@ from fastapi import HTTPException
 from tinydb import Query, TinyDB
 
 from app.config.config import settings
-from app.schemas.rules import RequestObject, UpdateRequestObject
-
 
 class PolicyDatabase:
     def __init__(self, database_url: str):
@@ -11,10 +9,9 @@ class PolicyDatabase:
         self.store = Query()
 
     def get_policy(self, policy_name: str, owner: str) -> dict:
-        policies = self.database.search(self.store.owner == owner)
-        for policy in policies:
-            if policy["name"] == policy_name:
-                return policy
+        policy = self.database.get((self.store.name == policy_name) & (self.store.owner == owner))
+        if policy:
+            return policy
         return {}
 
     def add_policy(self, policy: dict, owner: str) -> dict:
@@ -25,17 +22,18 @@ class PolicyDatabase:
         self.database.insert(policy)
         return policy
 
+    # Not working properly atm. TODO
     def update_policy(self, policy_name: str, policy: dict, owner: dict) -> None:
-        self.database.update(policy, self.store.name == policy_name and self.store.owner == owner)
+        self.database.update(policy, (self.store.name == policy_name) & (self.store.owner == owner))
 
     def exists(self, policy_name: str, owner: str) -> bool:
-        doc = self.database.get(self.store.name == policy_name and self.store.owner == owner)
+        doc = self.database.get((self.store.name == policy_name) & (self.store.owner == owner))
         if not doc:
             return False
         return doc["name"] == policy_name
 
     def delete_policy(self, policy_name: str, owner: str) -> None:
-        self.database.remove(self.store.name == policy_name and self.store.owner == owner)
+        self.database.remove((self.store.name == policy_name) & (self.store.owner == owner))
 
     def get_policies(self, owner: str) -> list:
         return self.database.search(self.store.owner == owner)
--- app/utils/write_rego.py
+++ app/utils/write_rego.py
@@ -1,4 +1,5 @@
 import os
+from functools import lru_cache
 
 from app.config.config import settings
 from app.server.github import GitHubOperations
@@ -7,10 +8,11 @@ from .build_rego_file import build_rego
 initiate_rule = "package httpapi.authz\nimport input\ndefault allow = false\n\n\n\n"
 
 
+@lru_cache(maxsize=1)
 class WriteRego:
     def __init__(self, access_token: str) -> None:
         self.access_token = access_token
-        self.github = GitHubOperations(settings.GITHUB_URL, access_token)
+        self.github = GitHubOperations(settings.GITHUB_URL, self.access_token)
 
     def write_to_file(self, rule: dict, operation: str = "write") -> None:
         """
@@ -46,7 +48,7 @@ class WriteRego:
     def delete_policy_file(self) -> bool:
         file_path = f"{self.github.local_repo_path}/auth.rego"
 
-        if not os.path.isfile(file_path):
+        if not os.path.exists(file_path):
             raise FileNotFoundError
 
         file = open(file_path, "w")
