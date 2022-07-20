842a92783f419dcaa4eedbb7e5c745016887e110
r-scheele <abdulrahmanolamilekan88@gmail.com>
2022-07-20 12:02:36 +0100

    [Update]

--- app/tests/test_get_repo_and_data.py
+++ app/tests/test_get_repo_and_data.py
@@ -1,8 +1,3 @@
 def test_get_public_and_private_repo_github(authorized_client):
     response = authorized_client.get(url="/user/repos/github")
     assert response.status_code == 200
-
-
-def test_get_data(authorized_client):
-    response = authorized_client.get(url="/data")
-    assert response.status_code == 200
