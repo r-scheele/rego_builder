ddaf767512e99a322d8c467d463981cea4219f71
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-08-05 20:22:24 +0100

    [Updates]
    - Integrate GitLab into the routes and introduced `provider`. The `provider` is to distinguish between the two git providers we support - GitHub and GitLabm and to provide appropriate support.

--- app/server/routes/policies.py
+++ app/server/routes/policies.py
@@ -4,6 +4,7 @@ from app.config.config import settings
 from app.database.policy import PolicyDatabase, get_db
 from app.schemas.rules import RequestObject, UpdateRequestObject
 from app.server.auth.authorize import TokenBearer
+from app.server.services.gitlab import GitLabOperations
 from app.utils.write_rego import WriteRego
 
 default_path = settings.BASE_PATH
@@ -13,17 +14,21 @@ router = APIRouter(tags=["Policy Operations"], prefix="/policies")
 
 @router.get("/")
 async def get_policies(
-    database: PolicyDatabase = Depends(get_db), dependencies=Depends(TokenBearer())
+        database: PolicyDatabase = Depends(get_db), dependencies=Depends(TokenBearer())
 ) -> list:
     return database.get_policies(dependencies["login"])
 
 
 @router.post("/")
 async def write_policy(
-    rego_rule: RequestObject,
-    database=Depends(get_db),
-    dependencies=Depends(TokenBearer()),
+        provider: str,
+        rego_rule: RequestObject,
+        database=Depends(get_db),
+        dependencies=Depends(TokenBearer()),
 ) -> dict:
+    if provider == "gitlab":
+        rego_rule.repo_url = GitLabOperations(rego_rule.repo_id, dependencies["token"]).repo_url_from_id()
+
     rego_rule.owner = dependencies["login"]
     policy = rego_rule.dict()
 
@@ -34,8 +39,20 @@ async def write_policy(
     policies.append(policy)
 
     # Write the policy to the database after successful push
+    if provider == "gitlab":
+        WriteRego(
+          access_token=dependencies["token"],
+          repo_url=rego_rule.repo_url,
+          username=dependencies["login"],
+          provider=provider,
+          repo_id=rego_rule.repo_id,
+        ).write_to_file(policies)
+        database.add_policy(policy, dependencies["login"])
+
+        return {"status": 200, "message": "Policy created successfully"}
+
     WriteRego(
-        dependencies["token"], policy["repo_url"], dependencies["login"]
+        dependencies["token"], policy["repo_url"], dependencies["login"], provider
     ).write_to_file(policies)
 
     database.add_policy(policy, dependencies["login"])
@@ -45,7 +62,7 @@ async def write_policy(
 
 @router.get("/{policy_id}")
 async def retrieve_policy(
-    policy_id: str, database=Depends(get_db), dependencies=Depends(TokenBearer())
+        policy_id: str, database=Depends(get_db), dependencies=Depends(TokenBearer())
 ) -> dict:
     stored_policy = database.get_policy(policy_id, dependencies["login"])
     if not stored_policy:
@@ -56,10 +73,11 @@ async def retrieve_policy(
 
 @router.put("/{policy_id}")
 async def modify_policy(
-    policy_id: str,
-    rego_rule: UpdateRequestObject,
-    database=Depends(get_db),
-    dependencies=Depends(TokenBearer()),
+        provider: str,
+        policy_id: str,
+        rego_rule: UpdateRequestObject,
+        database=Depends(get_db),
+        dependencies=Depends(TokenBearer()),
 ) -> dict:
     if not database.exists(policy_id, dependencies["login"]):
         raise HTTPException(status_code=404, detail="Policy not found")
@@ -74,10 +92,21 @@ async def modify_policy(
     updated_policy = database.get_policy(policy_id, dependencies["login"])
 
     policies = database.get_policies(dependencies["login"])
-    policies.append(updated_policy)
+
+    # Rewrite rego file and update Gitlab
+    if provider == "gitlab":
+        WriteRego(
+          access_token=dependencies["token"],
+          repo_url=updated_policy["repo_url"],
+          username=dependencies["login"],
+          provider=provider,
+          repo_id=updated_policy["repo_id"],
+        ).write_to_file(policies)
+        return {"status": 200, "message": "Updated successfully"}
+
     # Rewrite rego file and update GitHub
     WriteRego(
-        dependencies["token"], rego_rule["repo_url"], dependencies["login"]
+        dependencies["token"], updated_policy["repo_url"], dependencies["login"], provider
     ).write_to_file(policies)
 
     return {"status": 200, "message": "Updated successfully"}
@@ -85,20 +114,24 @@ async def modify_policy(
 
 @router.delete("/{policy_id}")
 async def remove_policy(
-    policy_id: str,
-    repo_url: str,
-    database=Depends(get_db),
-    dependencies=Depends(TokenBearer()),
+        provider: str,
+        policy_id: str,
+        repo_url: str,
+        database=Depends(get_db),
+        dependencies=Depends(TokenBearer()),
 ) -> dict:
     if not database.exists(policy_id, dependencies["login"]):
         raise HTTPException(status_code=404, detail="Policy not found")
 
     user = dependencies["login"]
+    repo_id = database.get_policy(policy_id, user)["repo_id"]
     # Remove policy from database
     database.delete_policy(policy_id, user, repo_url)
 
     # Update the policy in the rego file
     policies = database.get_policies(owner=user)
-    WriteRego(dependencies["token"], repo_url, user).write_to_file(policies)
+    if provider == "gitlab":
+        WriteRego(dependencies["token"], repo_id, user, provider).write_to_file(policies)
+    WriteRego(dependencies["token"], repo_url, user, provider).write_to_file(policies)
 
     return {"status": 200, "message": "Policy deleted successfully."}
