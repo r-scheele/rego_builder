95e406fe9c09d6a406c72720df26af6375b5babe
r-scheele <abdulrahmanolamilekan88@gmail.com>
2022-08-09 14:33:33 +0100

    Modified data response format, and queries

--- app/server/routes/data.py
+++ app/server/routes/data.py
@@ -1,11 +1,19 @@
+from app.schemas.data import DataRequest
 from fastapi import Depends, APIRouter
 
 from app.server.auth.authorize import TokenBearer
-from app.database.datasource_database import get_database
+
 
 router = APIRouter(tags=["Data Operations"])
 
 
-@router.get("/data")
-async def get_data(database=Depends(get_database)) -> dict:
-    return {"users": database.get_data()}
+@router.post("/data")
+async def get_data(data: DataRequest, dependencies=Depends(TokenBearer())) -> dict:
+    from app.database.datasource_database import get_database
+
+    res = {}
+    for data_query in data.queries:
+        sql = {}
+        sql[data_query.data_list_name] = data_query.sql_query
+        res.update(get_database().get_data(sql))
+    return {"data": res}
