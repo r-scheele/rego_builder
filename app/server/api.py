15422be683ebbda9f5779bc64ae3105e28c730f5
r-scheele <abdulrahmanolamilekan88@gmail.com>
2022-09-01 15:12:07 +0100

    [update] - reverted unnecessary file changes

--- app/server/api.py
+++ app/server/api.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from app.server.auth.get_token import router as auth_router
-from app.server.routes.data_routes import router as data_router
-from app.server.routes.policy_routes import router as api_router
-from app.server.routes.repo_routes import router as user_router
+from app.server.routes.data import router as data_router
+from app.server.routes.policy import router as api_router
+from app.server.routes.repo import router as user_router
 
 app = FastAPI(
     swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"},
