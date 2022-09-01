3db75b8b61f4e61598a24137c23ebf38dc172e61
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-09-01 16:13:49 +0100

    [Updates]
    - Update repo doc.

--- app/server/routes/repo.py
+++ app/server/routes/repo.py
@@ -5,12 +5,16 @@ import requests as r
 from fastapi import APIRouter, Depends
 
 from app.server.auth.authorize_token import TokenBearer
+from app.config.config import settings
 
 router = APIRouter(tags=["Repo Management"], prefix="/user/repos")
 
 
 @dataclass
 class RepoStructure:
+    """
+    Represents the structure of a repository.
+    """
     name: str
     id: int
     url: str
@@ -21,6 +25,11 @@ class RepoStructure:
 async def get_public_and_private_repo(
     dependencies=Depends(TokenBearer()),
 ) -> list:
+    """
+    Get all public and private repositories from GitHub
+
+    :param dependencies:
+    """
     url = f"https://api.github.com/search/repositories?q=user:{dependencies['login']}"
     repos = r.get(
         url=url,
@@ -42,20 +51,28 @@ async def get_public_and_private_repo(
 async def get_public_and_private_repo_gitlab(
     dependencies=Depends(TokenBearer()),
 ) -> list:
+    """
+    Get all public and private repositories from a GitLab organization.
+
+    :param dependencies: - token bearer object
+    :returns: list of repositories
+    """
     gl = gitlab.Gitlab("https://gitlab.com", oauth_token=dependencies["token"])
     gl.auth()
 
-    unfiltered_repos = gl.projects.list(owned=True)
     repos = []
 
-    for repo in unfiltered_repos:
-        r = repo.asdict()
+    glab_org_name = settings.ORG_NAME
+
+    group = gl.groups.get(glab_org_name).asdict()
+
+    for project in group["projects"]:
         repos.append(
             RepoStructure(
-                name=r["name"],
-                id=r["id"],
-                url=r["web_url"],
-                owner=r["namespace"]["full_path"],
+                name=project["name"],
+                id=project["id"],
+                url=project["web_url"],
+                owner=project["namespace"]["name"],
             )
         )
 
